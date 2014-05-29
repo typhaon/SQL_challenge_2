4cb42cb4c19e5a8d7fc70182f5a36848276d5424
Typhaon <davidgodwin788@gmail.com>
2014-05-29 12:09:47 -0400

    All core functionality is working.
    basic but functional.

--- server.rb
+++ server.rb
@@ -77,7 +77,7 @@ get '/movies/:id' do
 identifier = params[:id]
 
 query = "SELECT movies.title, genres.name AS genre, studios.name AS studio, actors.name,
-    cast_members.character
+    cast_members.character, actors.id
     FROM movies
     JOIN genres ON genres.id = movies.genre_id
     JOIN studios ON studios.id = movies.studio_id
@@ -85,9 +85,18 @@ query = "SELECT movies.title, genres.name AS genre, studios.name AS studio, acto
     JOIN actors ON actors.id = cast_members.actor_id
     WHERE movies.id = #{identifier}"
 
+movie_info_query = "SELECT movies.title, genres.name AS genre, studios.name AS studio
+    FROM movies
+    JOIN genres ON genres.id = movies.genre_id
+    JOIN studios ON studios.id = movies.studio_id
+    WHERE movies.id = #{identifier}"
 
 @results = db_connection do |conn|
   conn.exec(query)
+end
+
+@movie_results = db_connection do |conn|
+  conn.exec(movie_info_query)[0]
 end
 
   erb :'movies/show'
